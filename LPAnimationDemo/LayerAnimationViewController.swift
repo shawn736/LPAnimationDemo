af2684a79a3ee785af3ca2b13ff165dbd496391f
shawn.xiao <xwh736@163.com>
2018-11-05 10:16:08 +0800

    add layer animation

--- LPAnimationDemo/LayerAnimationViewController.swift
+++ LPAnimationDemo/LayerAnimationViewController.swift
@@ -56,6 +56,17 @@ class LayerAnimationViewController: BaseViewController {
      borderWidth
      cornerRadius
      */
+    storyImageLayer.borderColor = UIColor.blue.cgColor
+    let animator = CABasicAnimation(keyPath: "borderWidth")
+    animator.fromValue = 0.0
+    animator.toValue = 10.0
+    animator.duration = duration*4
+    // 方案一，以下保持动画结束时的状态，如果没加默认是还是动画开始前的状态
+    animator.fillMode = .forwards
+    animator.isRemovedOnCompletion = false
+    
+    storyImageLayer.add(animator, forKey: nil)
+//    storyImageLayer.borderWidth = 10.0 // 方案二，这里相当于在动画开始前就调整了状态，这样动画结束后就是想要的结果
   }
   
   func shadowAnimation() {
@@ -65,6 +76,15 @@ class LayerAnimationViewController: BaseViewController {
      shadowPath
      shadowRadius
      */
+    storyImageLayer.shadowOpacity = 1.0
+    storyImageLayer.shadowColor = UIColor.gray.cgColor
+    let animator = CABasicAnimation(keyPath: "shadowOffset")
+    animator.fromValue = CGSize(width: 0, height: 0)
+    animator.toValue = CGSize(width: 20, height: 20)
+    animator.duration = duration*4
+    animator.fillMode = .forwards
+    animator.isRemovedOnCompletion = false
+    storyImageLayer.add(animator, forKey: nil)
   }
   
   func contentsAnimation() {
@@ -73,6 +93,12 @@ class LayerAnimationViewController: BaseViewController {
      mask
      opacity
      */
+    let animator = CABasicAnimation(keyPath: "contents")
+    animator.toValue = UIImage(named: "plane")?.cgImage
+    animator.duration = duration*4
+    animator.fillMode = .forwards
+    animator.isRemovedOnCompletion = false
+    storyImageLayer.add(animator, forKey: nil)
   }
   
   
