b74124269cba7e924c47bedce106681d52782789
shawn.xiao <xwh736@163.com>
2018-11-05 10:30:13 +0800

    updates

--- LPAnimationDemo/LayerAnimationViewController.swift
+++ LPAnimationDemo/LayerAnimationViewController.swift
@@ -36,6 +36,15 @@ class LayerAnimationViewController: BaseViewController {
     }
   }
   
+  /*
+   layer 的属性有三个概念：original value - 添加动画前的属性值; from value - 动画的初始值; to value - 动画的结束值.
+   fillMode的值会影响到layer在动画前和动画后的行为，有4中可设置的属性值：
+   kCAFillModeRemoved ：begin 以前一直是original value, 动画开始时属性值立马切换到from value, 然后动画变化到 to value, 动画结束时属性值又立马切换到original value. (original value 和 from value 值不同时，可以明显看出来)
+   kCAFillModeBackwards：动画前就切换到from value了，延迟几秒执行动画可以明显看出效果
+   kCAFillModeForwards：动画结束后一直保持在to value, 起作用前提是removedOnCompletion设置为false
+   kCAFillModeBoth：动画前就切换到from value, 动画后一直保持在to value, 起作用前提是removedOnCompletion设置为false
+   */
+  
   func positionAndSizeAnimation() {
     /*
      bounds
@@ -45,7 +54,9 @@ class LayerAnimationViewController: BaseViewController {
     let animator = CABasicAnimation(keyPath: "position.y")
     animator.fromValue = storyImageLayer.position.y - 100
     animator.toValue = storyImageLayer.position.y + 400
+    animator.beginTime = CACurrentMediaTime() + 2 // 把动画延迟 2秒执行
     animator.duration = duration*4
+    animator.fillMode = .backwards
     storyImageLayer.add(animator, forKey: nil)
   
   }
@@ -66,7 +77,7 @@ class LayerAnimationViewController: BaseViewController {
     animator.isRemovedOnCompletion = false
     
     storyImageLayer.add(animator, forKey: nil)
-//    storyImageLayer.borderWidth = 10.0 // 方案二，这里相当于在动画开始前就调整了状态，这样动画结束后就是想要的结果
+//    storyImageLayer.borderWidth = 10.0 // 方案二，这里相当于在动画开始前就调整了状态，即修改了original value，这样动画结束后就是想要的结果
   }
   
   func shadowAnimation() {
